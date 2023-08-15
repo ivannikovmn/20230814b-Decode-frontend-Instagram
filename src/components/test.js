2834946ef69a65a87ceb14f73fd0ccaa583f2dc0
ivannikovmn <local_host@mail.ru>
2023-08-15 14:34:53 +0600

    initial

---
+++ src/components/test.js
@@ -0,0 +1,9 @@
+export default function Test(){
+    const name = "Mikhail"
+
+    return (<div>
+        <h1>My test Component {name} </h1>
+        <p>My test parag ...</p>
+        <a>test link</a>
+    </div>)
+}
