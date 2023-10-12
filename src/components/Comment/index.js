517b5043ff820dc2a537776827c53ecef1fa5bd0
ivannikovmn <local_host@mail.ru>
2023-10-12 20:47:42 +0600

    fix сomment

--- src/components/Comment/index.js
+++ src/components/Comment/index.js
@@ -2,7 +2,7 @@ export default function Comment({comment, remove}) {
     return(
         <div>            
                 terrylucas: 
-                {comment.comment}
+                {comment}
             <span onClick={()=>remove(comment)} style={{cursor:'pointer'}}>(Удалить)</span>
         </div>           
   )
